2b15eb131ba83a5eb35cbf55b39224411bba4bb6
Gabriel Dias <gabrieldiasrosa@hotmail.com>
2022-03-13 17:40:03 -0300

    First release.

---
+++ Cheats/_BaseCheat.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using MelonLoader;
+
+namespace YuGiOh_Master_Duel.BaseClasses
+{
+    abstract class _BaseCheat : iBaseCheat
+    {
+        protected int sleep = 1000;
+        protected Boolean loop = true;
+        protected Boolean enabled = false;
+        protected Thread thread;
+
+        protected _BaseCheat()
+        {
+            MelonLogger.Msg("_BaseCheat._BaseCheat()");
+            this.thread = new Thread(new ThreadStart(run));            
+        }
+
+        protected _BaseCheat(int sleep)
+        {
+            MelonLogger.Msg("_BaseCheat._BaseCheat()");
+            MelonLogger.Msg("_BaseCheat._BaseCheat.sleep: " + sleep);
+
+            this.sleep = sleep;
+            this.thread = new Thread(new ThreadStart(run));
+        }
+
+        public Boolean scriptToggle()
+        {            
+            this.enabled = !this.enabled;
+            MelonLogger.Msg("_BaseCheat.scriptToggle: " + this.enabled);
+            return this.enabled;
+        }
+
+        public void scriptEnable()
+        {            
+            this.enabled = true;
+            MelonLogger.Msg("_BaseCheat.scriptEnable: " + this.enabled);
+        }
+
+        public void scriptDisable()
+        {
+            this.enabled = false;
+            MelonLogger.Msg("_BaseCheat.scriptDisable: " + this.enabled);
+        }
+
+        public void loopStart()
+        {
+            if(this.thread.ThreadState == ThreadState.Unstarted)
+            {
+                this.loop = true;                
+            }
+            MelonLogger.Msg("_BaseCheat.loopStart: " + this.loop);
+            if (this.loop)
+            {
+                this.thread.Start();
+            }
+        }
+
+        public void loopEnd()
+        {
+            this.scriptDisable();
+            this.loop = false;
+            MelonLogger.Msg("_BaseCheat.loopEnd: " + this.loop);
+        }
+
+        public abstract void run();
+    }
+
+    interface iBaseCheat
+    {
+        void run();
+    }
+}
---
+++ MasterDuel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using YgomGame.Duel;
+
+namespace YuGiOh_Master_Duel
+{
+    public class MasterDuel : MelonMod
+    {
+        public MasterDuel()
+        {
+            MelonLogger.Msg("MasterDuel.MasterDuel()");
+            MelonLogger.Msg("F12: Surrender Enemy");
+            MelonLogger.Msg("KeyPad+: Increase Game Speed");
+            MelonLogger.Msg("KeyPad-: Decrease Game Speed");
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F12))
+            {
+                MelonLogger.Msg(">> Engine.DLL_DuelComDoCommand");
+                Engine.DLL_DuelComDoCommand(Engine.Rival(), 0, 0, 11);
+            }
+            else if (Input.GetKeyDown(KeyCode.KeypadPlus))
+            {                
+                float timeScale = Time.timeScale;
+                Time.timeScale++;
+                MelonLogger.Msg(">> Time.timeScale: " +  timeScale + " -> " + Time.timeScale);
+            }
+            else if (Input.GetKeyDown(KeyCode.KeypadMinus))
+            {
+                float timeScale = Time.timeScale;
+                Time.timeScale--;
+                MelonLogger.Msg(">> Time.timeScale: " + timeScale + " -> " + Time.timeScale);
+
+            }
+        }
+    }
+}
